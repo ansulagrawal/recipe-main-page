0c567b5248cd1e650ebd0a66a0832bf75fee4f7c
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-04-06 17:16:08 +0530

    Completed Design

--- app/page.js
+++ app/page.js
@@ -2,111 +2,136 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+    <main className="w-full md:w-2/3 md:my-32 md:p-10 m-auto flex flex-col justify-center bg-white rounded-none md:rounded-3xl overflow-hidden">
+      <div className="h-40 md:h-96 relative mb-5 overflow-hidden  rounded-none md:rounded-3xl ">
+        <Image src="/omelette.jpeg" fill alt="Omelette" />
+      </div>
+      <div className="px-8 md:px-0">
+        <h1 className=" text-3xl font-bold text-dark-charcoal">
+          Simple Omelette Recipe
+        </h1>
+        <p className="my-3">
+          An easy and quick dish, perfect for any meal. This classic omelette
+          combines beaten eggs cooked to perfection, optionally filled with your
+          choice of cheese, vegetables, or meats.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <section id="preparation-time" className="bg-rose-white p-6 my-4">
+          <ul>
+            <label className="text-primary-2 font-bold text-lg">
+              Preparation time
+            </label>
+            <li>
+              <b>Total</b>: Approximately 10 minutes
+            </li>
+            <li>
+              <b>Preparation</b>: 5 minutes
+            </li>
+            <li>
+              <b>Cooking</b>: 5 minutes
+            </li>
+          </ul>
+        </section>
+        <section id="ingredients" className="my-5">
+          <h2 className="text-primary-1 font-bold text-3xl mb-5">
+            Ingredients
+          </h2>
+          <ol>
+            <li>2-3 large eggs</li>
+            <li>Salt, to taste</li>
+            <li>Pepper, to taste</li>
+            <li>1 tablespoon of butter or oil</li>
+            <li>
+              Optional fillings: cheese, diced vegetables, cooked meats, herbs
+            </li>
+          </ol>
+        </section>
+        <div className="h-[1px] bg-light-grey my-5" />
+        <section id="instructions" className="my-5">
+          <h2 className="text-primary-1 font-bold text-3xl mb-5">
+            Instructions
+          </h2>
+          <ol className="flex flex-col gap-2">
+            <li>
+              <b>Beat the eggs:</b> In a bowl, beat the eggs with a pinch of
+              salt and pepper until they are well mixed. You can add a
+              tablespoon of water or milk for a fluffier texture.
+            </li>
+            <li>
+              <b>Heat the pan:</b> Place a non-stick frying pan over medium heat
+              and add butter or oil.
+            </li>
+            <li>
+              Cook the omelette: Once the butter is melted and bubbling, pour in
+              the eggs. Tilt the pan to ensure the eggs evenly coat the surface.
+            </li>
+            <li>
+              <b>Add fillings (optional):</b> When the eggs begin to set at the
+              edges but are still slightly runny in the middle, sprinkle your
+              chosen fillings over one half of the omelette.
+            </li>
+            <li>
+              <b>Fold and serve:</b> As the omelette continues to cook,
+              carefully lift one edge and fold it over the fillings. Let it cook
+              for another minute, then slide it onto a plate.
+            </li>
+            <li>
+              <b>Enjoy:</b> Serve hot, with additional salt and pepper if
+              needed.
+            </li>
+          </ol>
+        </section>
+        <div className="h-[1px] bg-light-grey my-5" />
+
+        <section id="nutrition" className="my-5">
+          <h2 className="text-primary-1 font-bold text-3xl mb-5">Nutrition</h2>
+          <p className="mb-3">
+            The table below shows nutritional values per serving without the
+            additional fillings.
+          </p>
+
+          <table className="w-full">
+            <tbody className="w-full">
+              <tr className="border-b-[1px] border-b-light-grey w-full">
+                <td className="w-[50%] py-2 text-gray-700">Calories</td>
+                <td className="w-[50%] py-2 text-primary-2 font-extrabold">
+                  277kcal
+                </td>
+              </tr>
+
+              <tr className="border-b-[1px] border-b-light-grey w-full">
+                <td className="w-[50%] py-2 text-gray-700">Carbs</td>
+                <td className="w-[50%] py-2 text-primary-2 font-extrabold">
+                  0g
+                </td>
+              </tr>
+
+              <tr className="border-b-[1px] border-b-light-grey w-full">
+                <td className="w-[50%] py-2 text-gray-700">Protein</td>
+                <td className="w-[50%] py-2 text-primary-2 font-extrabold">
+                  20g
+                </td>
+              </tr>
+
+              <tr className="border-b-[1px] border-b-light-grey w-full">
+                <td className="w-[50%] py-2 text-gray-700">Fat</td>
+                <td className="w-[50%] py-2 text-primary-2 font-extrabold">
+                  22g
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <div className="attribution mt-20">
+          Challenge by{" "}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.frontendmentor.io?ref=challenge"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Frontend Mentor
           </a>
+          . Coded by <a href="https://www.frontendmentor.io/profile/ansulagrawal" target="_blank" rel="author">Ansul Agrawal</a>.
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "rose-white": "hsl(330, 100%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+        eggshell: "hsl(30, 54%, 90%)",
+        "light-grey": "hsl(30, 18%, 87%)",
+        "wenge-brown": "hsl(30, 10%, 34%)",
+        "dark-charcoal": "hsl(24, 5%, 18%)",
+        "primary-1": "hsl(14, 45%, 36%)",
+        "primary-2": "hsl(332, 51%, 32%)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
